f68e31b2bf143942df51af51d2e12ed1a50c4722
duc-desktop <englishlayup@protonmail.com>
2022-05-24 14:28:14 -0400

    change indentations from spaces to tabs

--- src/publishPost.ts
+++ src/publishPost.ts
@@ -1,43 +1,43 @@
-import fs from "fs";
-import matter from "gray-matter";
-import path from "path";
-import { argv, exit } from "process";
-import { createPost } from "./redis";
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import { argv, exit } from 'process';
+import { createPost } from './redis';
 
-const postsDir = path.join(process.cwd(), "posts");
+const postsDir = path.join(process.cwd(), 'posts');
 
 interface Post {
-  title: string;
-  date: Date;
-  description: string;
-  content: string;
-  filename: string;
+	title: string;
+	date: Date;
+	description: string;
+	content: string;
+	filename: string;
 }
 
 async function publish(filename: string) {
-  let fullPath: string;
+	let fullPath: string;
 
-  if (filename.endsWith(".md")) {
-    fullPath = path.join(postsDir, `${filename}`);
-  } else {
-    fullPath = path.join(postsDir, `${filename}.md`);
-  }
+	if (filename.endsWith('.md')) {
+		fullPath = path.join(postsDir, `${filename}`);
+	} else {
+		fullPath = path.join(postsDir, `${filename}.md`);
+	}
 
-  const fileContent = fs.readFileSync(fullPath, "utf8");
+	const fileContent = fs.readFileSync(fullPath, 'utf8');
 
-  const matterResult = matter(fileContent);
+	const matterResult = matter(fileContent);
 
-  const post: Post = {
-    title: matterResult.data.title,
-    date: new Date(),
-    description: matterResult.data.description,
-    content: matterResult.content,
-    filename: filename,
-  };
+	const post: Post = {
+		title: matterResult.data.title,
+		date: new Date(),
+		description: matterResult.data.description,
+		content: matterResult.content,
+		filename: filename,
+	};
 
-  const id = await createPost(post);
+	const id = await createPost(post);
 
-  console.log(id);
+	console.log(id);
 }
 
 publish(argv[2]).then(() => exit());
--- src/redis.ts
+++ src/redis.ts
@@ -1,53 +1,53 @@
-import { config } from "dotenv";
-import { Client, Entity, Schema } from "redis-om";
+import { config } from 'dotenv';
+import { Client, Entity, Schema } from 'redis-om';
 
 const client = new Client();
 
 async function connect() {
-  if (!client.isOpen()) {
-    config();
-    await client.open(process.env.REDIS_URL);
-  }
+	if (!client.isOpen()) {
+		config();
+		await client.open(process.env.REDIS_URL);
+	}
 }
 
 interface Post {
-  title: string;
-  date: Date;
-  description: string;
-  content: string;
-  filename: string;
+	title: string;
+	date: Date;
+	description: string;
+	content: string;
+	filename: string;
 }
 
 class Post extends Entity {}
 
 const schema = new Schema(
-  Post,
-  {
-    title: { type: "text" },
-    date: { type: "date", sortable: true },
-    description: { type: "text" },
-    content: { type: "string" },
-    filename: { type: "string" },
-  },
-  {
-    dataStructure: "JSON",
-  }
+	Post,
+	{
+		title: { type: 'text' },
+		date: { type: 'date', sortable: true },
+		description: { type: 'text' },
+		content: { type: 'string' },
+		filename: { type: 'string' },
+	},
+	{
+		dataStructure: 'JSON',
+	}
 );
 
 export async function createPost(data: {
-  title: string;
-  date: Date;
-  description: string;
-  content: string;
-  filename: string;
+	title: string;
+	date: Date;
+	description: string;
+	content: string;
+	filename: string;
 }) {
-  await connect();
+	await connect();
 
-  const repository = client.fetchRepository(schema);
+	const repository = client.fetchRepository(schema);
 
-  const post = repository.createEntity(data);
+	const post = repository.createEntity(data);
 
-  const id = await repository.save(post);
+	const id = await repository.save(post);
 
-  return id;
+	return id;
 }
